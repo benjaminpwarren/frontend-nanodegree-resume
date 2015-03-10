0595de7c0b69161fee43cba445462dc6d8dab597
Benjamin Warren <benjaminpwarren@gmail.com>
2015-03-10 17:54:56 +1100

    Create and populate bio, education, work, and projects objects in resumeBuilder.
    Delete Hello World! from index.html.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,300 @@
+var bio = {
+  "name"    : "Benjamin Warren",
+  "role"    : "Senior Analyst Developer & Stay-at-home Dad",
+  "picURL"  : "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/0fd/161/2cee61c.jpg",
+  "welcomeMessage"  : "Hey, thanks for taking the time to checkout my resume.",
+  "skills"  : [
+      "SQL",
+      "JavaSript",
+      "HTML",
+      "SharePoint",
+      "T-SQL",
+      "Teradata SQL",
+      "MS Office Suite",
+      "VBA",
+      "Requirements Analysis",
+      "Databases",
+      "Database Design",
+      "Business Intelligence",
+      "SDLC"
+  ],
+  "contacts": {
+      "mobile"  : "0421035676",
+      "email"   : "benjaminpwarren+udacity@gmail.com",
+      "github"  : "benjaminpwarren",
+      "twitter" : "benjaminpwarren"
+  }
+};
+
+var work = {
+  "jobs":[
+    {
+      "employer": "Domino's Pizza",
+      "title"   : "Delivery Driver",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "1999-2000 (1 year)",
+      "description": "Delivered pizzas, prepped dough, cleaned, ran dispatch, and helped close-up."
+    },
+    {
+      "employer": "OzEmail",
+      "title"   : "Technical Support Officer",
+      "location": "Sydney, NSW, Australia",
+      "dates"   : "April 2000 - August 2000 (5 months)",
+      "description": "Helped customers with internet connectivity and configuration issues. (In the days of dial-up!)"
+    },
+    {
+      "employer": "TeleTech",
+      "title"   : "Customer Service Officer",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "March 2001 - October 2001 (8 months)",
+      "description": "Made domestic flight reservations for Qantas Frequent Flyer customers."
+    },
+    {
+      "employer": "TeleTech",
+      "title"   : "Intranet Developer",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "October 2001 - August 2002 (11 months)",
+      "description": "Developed and maintained an Activity & Call Disposition system with reporting using classic ASP, HTML, JavaScript, and MS Excel."
+    },
+    {
+      "employer": "Commonwealth Bank of Australia",
+      "title"   : "Customer Service Officer",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "August 2002 - May 2004 (1 year 10 months)",
+      "description": "Helped customers with Direct Banking enquires (general banking and credit cards)."
+    },
+    {
+      "employer": "Child Support Agency",
+      "title"   : "Customer Service Officer",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "May 2004 - March 2005 (11 months)",
+      "description": "Assessed customers for Child Support (maintenance) and collected unpaid maintenance."
+    },
+    {
+      "employer": "Child Support Agency",
+      "title"   : "Business Analyst (State)",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "March 2005 - November 2006 (1 year 9 months)",
+      "description": "Developed reporting, workload delivery, and automation systems using HTML, JavaScript, and VBA."
+    },
+    {
+      "employer": "Child Support Agency",
+      "title"   : "Business Analyst",
+      "location": "Canberra, ACT, Australia",
+      "dates"   : "November 2006 - April 2009 (2 years 6 months)",
+      "description": "Targetted customer selection for workload delivery using T-SQL, and report development using Cognos, T-SQL, and VBA."
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Senior Business Analyst",
+      "location": "Canberra, ACT, Australia",
+      "dates"   : "April 2009 - December 2011 (2 years 9 months)",
+      "description": "Targetted customer selection for workload delivery using T-SQL, and report development using Cognos, T-SQL, and VBA. Staff management."
+    },
+    {
+      "employer": "Australian Prudential Regulation Authority",
+      "title"   : "Contract Developer",
+      "location": "Canberra, ACT, Australia & Newcastle, NSW, Australia",
+      "dates"   : "December 2009 - November 2011 (1 year, 11 months)",
+      "description": "Developed decision review system (MS Access) and a mobile quality assurance recording system (Windows Mobile 6)"
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Senior Project Officer",
+      "location": "Newcastle, NSW, Australia",
+      "dates"   : "December 2011 - June 2012 (7 months)",
+      "description": "Forecasted workload capacity (workload vs. staffing etc)."
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Senior Business Analyst / Developer, Assistant Director",
+      "location": "Brisbane, Qld, Australia",
+      "dates"   : "July 2012 - September 2013 (1 year 3 months)",
+      "description": "Responded to ad hoc data requests. Data sourced using primarily T-SQL and provided with detailed documentation and guidance."
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Acting Director",
+      "location": "Brisbane, Qld, Australia",
+      "dates"   : "January 2013 - March 2013 (3 months)",
+      "description": "Managed a section of 25 ex-Child Support, ex-Medicare, and ex-Centrelink staff in our Information Management division."
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Senior Analyst / Developer, Assistant Director",
+      "location": "Brisbane, Qld, Australia & Newcastle, NSW, Australia",
+      "dates"   : "September 2013 - Present (1 year 7 months)",
+      "description": "Development and maintenance of our internal division workload management system using SharePoint with forms customised using JavaScript and HTML."
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Acting Director",
+      "location": "Brisbane, Qld, Australia",
+      "dates"   : "October 2013 - December 2013 (3 months)(concurrent with other role)",
+      "description": "Established and managed a quality, learning, and development team to improve the quality of the division through staff training, knowledge sharing, and quality assurance."
+    },
+    {
+      "employer": "Department of Human Services",
+      "title"   : "Senior Project Officer",
+      "location": "Brisbane, Qld, Australia & Canberra, ACT, Australia",
+      "dates"   : "January 2014 - June 2014 (1 year)(concurrent with other role)",
+      "description": "Provided expert knowledge of our CRM's data and data structures to facilate migration to a replacement system."
+    }
+  ]
+}
+
+var education = {
+  "schools": [
+    {
+      "name"  : "Wallsend South Public School",
+      "suburb": "Elermore Vale, NSW, Australia",
+      "years" : "1987-1987",
+      "degree": "Primary"
+    },
+    {
+      "name"  : "Moree Public School",
+      "suburb": "Moree, NSW, Australia",
+      "years" : "1987-1988",
+      "degree": "Primary"
+    },
+    {
+      "name"  : "Brooklyn Public School",
+      "suburb": "Brooklyn, NSW, Australia",
+      "years" : "1988-1989",
+      "degree": "Primary"
+    },
+    {
+      "name"  : "Charlestown East Public School",
+      "suburb": "Charlestown, NSW, Australia",
+      "years" : "1989-1989",
+      "degree": "Primary"
+    },
+    {
+      "name"  : "Cardiff Public School",
+      "suburb": "Cardiff, NSW, Australia",
+      "years" : "1990-1993",
+      "degree": "Primary"
+    },
+    {
+      "name"  : "Merewether High School",
+      "suburb": "Broadmeadow, NSW, Australia",
+      "years" : "1994-1999",
+      "degree": "Secondary"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "course": "Front-End Web Developer Nanodegree",
+      "school": "Udacity",
+      "years" : "2015-2015",
+      "url"   : "https://www.udacity.com/course/nd001"
+    },
+    {
+      "course": "JavaScript Basics",
+      "school": "Udacity",
+      "years" : "2015-2015",
+      "url"   : "https://www.udacity.com/course/ud804"
+    },
+    {
+      "course": "How to Use Git and GitHub",
+      "school": "Udacity",
+      "years" : "2015-2015",
+      "url"   : "https://www.udacity.com/course/ud775"
+    }
+  ]
+}
+
+var projects : {
+  projects: [
+    {
+      "title": "P1: Mockup to Web Site",
+      "dates": "2015-02-01 - 2015-02-13",
+      "description":"Built a website based on the provided mockup.",
+      "images": ['images/p1-mock-web.jpg']
+    }
+  ]
+}
+
+/*
+
+var addBio = (function(){
+  var $header = $("#header");
+
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+  // add name as first child of header, and role after name.
+  // changed as prepending twice is silly
+  $(formattedName).prependTo($header).after(formattedRole);
+  //$("#header").prepend(formattedRole);
+  //$("#header").prepend(formattedName);
+
+  var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+  var formattedEmail = HTMLemail.replace("%data%", bio.email);
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.picURL);
+  var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.msg );
+
+  var $topContacts =  $("#topContacts");
+  $(formattedBioPic).insertBefore($topContacts);
+  $(formattedWelcomeMsg).insertBefore($topContacts);
+
+  $(formattedMobile).appendTo($topContacts);
+  $(formattedEmail).appendTo($topContacts);
+
+  // add skills
+  var addSkills = (function(){
+
+    $(HTMLskillsStart).insertBefore($topContacts);
+    var $skills = $("#skills");
+    console.log()
+
+    var iSkills = bio.skills.length;
+    for (var iSkill = 0; iSkill < iSkills; iSkill++){
+
+      var formattedSkill = HTMLskills.replace("%data%", bio.skills[iSkill]);
+      console.log(formattedSkill);
+      $skills.append(formattedSkill);
+    }
+    $skills.insertBefore($topContacts);
+
+  })();
+
+})();
+
+var addWork = (function(){
+
+  //var $workExperience = $("#workExperience");
+
+  var $workEntry = $(HTMLworkStart);
+
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.position);
+  var formattedWorkDates = HTMLworkDates.replace("%data%", work.years);
+  var formattedLocation = HTMLworkLocation.replace("%data%", work.citiesStr);
+
+  $(formattedEmployer+formattedTitle).appendTo($workEntry);
+  $(formattedWorkDates).appendTo($workEntry);
+  $(formattedLocation).appendTo($workEntry);
+
+  $workEntry.appendTo("#workExperience");
+
+})();
+
+var addEducation = (function(){
+
+  var $educationEntry = $(HTMLschoolStart);
+
+
+  var formattedSchoolName = HTMLschoolName.replace("%data%", education["schoolName"]);
+  var formattedDegree = HTMLschoolDegree.replace("%data%", education["degree"]);
+  var formattedDates = HTMLschoolDates.replace("%data%", education["years"]);
+  var formattedCity = HTMLschoolLocation.replace("%data%", education["city"]);
+
+  $(formattedSchoolName + formattedDegree).appendTo($educationEntry);
+  $(formattedDates).appendTo($educationEntry);
+  $(formattedCity).appendTo($educationEntry);
+
+  $educationEntry.appendTo("#education");
+})();
+
+*/
